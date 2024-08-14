e410fded9e77efaf9bd1caf138dad3a2a9f2ff83
Wojciech <thaazar@gmail.com>
2024-08-14 13:10:56 +0200

    LW-005-ShipmentInfo Add communication between apps about shipment status

--- src/main/java/com/wojciechbarwinski/demo/legendary_warehouse/controllers/OrderController.java
+++ src/main/java/com/wojciechbarwinski/demo/legendary_warehouse/controllers/OrderController.java
@@ -2,24 +2,26 @@ package com.wojciechbarwinski.demo.legendary_warehouse.controllers;
 
 
 import com.wojciechbarwinski.demo.legendary_warehouse.dtos.OrderDTO;
+import com.wojciechbarwinski.demo.legendary_warehouse.dtos.ShipmentInfo;
 import com.wojciechbarwinski.demo.legendary_warehouse.services.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class OrderController {
 
 
     private final OrderService orderService;
+    private final WebClient webClient;
 
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
 
     @PostMapping("/order")
     public ResponseEntity<Void> correctOrder(@RequestBody OrderDTO order) {
@@ -28,4 +30,20 @@ public class OrderController {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @PostMapping("/order/shipment")
+    public ResponseEntity<String> correctOrder(@RequestBody ShipmentInfo shipmentInfo) {
+
+        String url = "http://localhost:8080/shipment";
+
+        String responseBody = webClient.post()
+                .uri(url)
+                .bodyValue(shipmentInfo)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return ResponseEntity.ok("Paczka została wysłana do aplikacji B: " + responseBody); //TODO what to return when something goes wrong?
+    }
+
 }
